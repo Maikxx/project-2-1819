5f9d1f562d238b4c661a6268a76ce35a9c1098a4
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-04 18:48:24 +0200

    feat(client/js): enhance user experience with javascript

--- client/scripts/room.ts
+++ client/scripts/room.ts
@@ -1,71 +1,93 @@
-function featureExists(feature: string, where: any, type?: string) {
-    return feature in where
-        && type ?
-            typeof where[feature] === type
-            : true
-}
+(() => {
+    function featureExists(feature: string, where: any, type?: string) {
+        return feature in where
+            && type ?
+                typeof where[feature] === type
+                : true
+    }
 
-if (
-    featureExists('querySelectorAll', document.body, 'function')
-    && featureExists('forEach', Array.prototype, 'function')
-    && featureExists('addEventListener', document.body, 'function')
-    && featureExists('setAttribute', document.body, 'function')
-    && featureExists('hash', window.location, 'string')
-    && featureExists('replace', String.prototype, 'function')
-    && featureExists('removeAttribute', document.body, 'function')
-    && featureExists('key', KeyboardEvent.prototype, 'string')
-) {
-    const modals: NodeListOf<Element> = document.querySelectorAll('.Modal')
+    if (
+        featureExists('querySelectorAll', document.body, 'function')
+        && featureExists('forEach', Array.prototype, 'function')
+        && featureExists('addEventListener', document.body, 'function')
+        && featureExists('setAttribute', document.body, 'function')
+        && featureExists('hash', window.location, 'string')
+        && featureExists('replace', String.prototype, 'function')
+        && featureExists('removeAttribute', document.body, 'function')
+    ) {
+        const modals: NodeListOf<Element> = document.querySelectorAll('.Modal')
 
-    turnOffTabIndexForOffScreenElements(modals)
-    activateTabIndexOnHashChange(modals)
-}
+        turnOffTabIndexForOffScreenElements(modals)
+        activateTabIndexOnHashChange(modals)
+    }
 
-function turnOffTabIndexForOffScreenElements(modals: NodeListOf<Element>) {
-    if (modals && modals.length > 0) {
-        modals.forEach((modal: Element) => {
-            const interactiveElements: NodeListOf<Element> = modal.querySelectorAll('button, a, input, textarea')
+    function turnOffTabIndexForOffScreenElements(modals: NodeListOf<Element>) {
+        if (modals && modals.length > 0) {
+            modals.forEach((modal: Element) => {
+                const interactiveElements: NodeListOf<Element> = modal.querySelectorAll('button, a, input, textarea')
 
-            if (interactiveElements && interactiveElements.length > 0) {
-                interactiveElements.forEach((interactiveElement: Element) => {
-                    interactiveElement.setAttribute('tabindex', '-1')
+                if (interactiveElements && interactiveElements.length > 0) {
+                    interactiveElements.forEach((interactiveElement: Element) => {
+                        interactiveElement.setAttribute('tabindex', '-1')
+                    })
+                }
+            })
+        }
+    }
+
+    function activateTabIndexOnHashChange(modals: NodeListOf<Element>) {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash
+            const pageOffset = getCurrentScrollOffset()
+
+            if (hash && hash.length > 0) {
+                modals.forEach((modal: Element) => {
+                    const id = hash.replace('#', '')
+
+                    if (modal.id === id) {
+                        const interactiveElements: NodeListOf<Element> = modal.querySelectorAll('button, a, input, textarea')
+                        const closeModalButton: Element = modal.querySelector('.Modal__close-button')
+
+                        if (interactiveElements && interactiveElements.length > 0) {
+                            interactiveElements.forEach((interactiveElement: Element) => {
+                                interactiveElement.removeAttribute('tabindex')
+                            })
+                        }
+
+                        if (closeModalButton) {
+                            closeModalButton.addEventListener('click', event => {
+                                // This is done to create the same experience as with pressing the escape key.
+                                event.preventDefault()
+                                window.location.hash = '#'
+
+                                if (typeof pageOffset === 'number') {
+                                    window.scrollTo(0, pageOffset)
+                                }
+
+                                closeModalButton.setAttribute('tabindex', '-1')
+                            })
+                        }
+                    }
+                })
+
+                window.addEventListener('keydown', event => {
+                    if (typeof event.key === 'string' && (event.key === 'Esc' || event.key === 'Escape')) {
+                        window.location.hash = '#'
+
+                        if (typeof pageOffset === 'number') {
+                            window.scrollTo(0, pageOffset)
+                        }
+                    }
                 })
             }
         })
     }
-}
 
-function activateTabIndexOnHashChange(modals: NodeListOf<Element>) {
-    window.addEventListener('hashchange', () => {
-        const hash = window.location.hash
-
-        if (hash && hash.length > 0) {
-            modals.forEach((modal: Element) => {
-                const id = hash.replace('#', '')
-
-                if (modal.id === id) {
-                    const interactiveElements: NodeListOf<Element> = modal.querySelectorAll('button, a, input, textarea')
-                    const closeModalButton: Element = modal.querySelector('.Modal__close-button')
-
-                    if (interactiveElements && interactiveElements.length > 0) {
-                        interactiveElements.forEach((interactiveElement: Element) => {
-                            interactiveElement.removeAttribute('tabindex')
-                        })
-                    }
-
-                    if (closeModalButton) {
-                        closeModalButton.addEventListener('click', () => {
-                            closeModalButton.setAttribute('tabindex', '-1')
-                        })
-                    }
-                }
-            })
-
-            window.addEventListener('keydown', event => {
-                if (event.key === 'Esc' || event.key === 'Escape') {
-                    window.location.hash = '#'
-                }
-            })
-        }
-    })
-}
+    function getCurrentScrollOffset() {
+        return featureExists('scrollY', window, 'number')
+            ? window.scrollY
+            : featureExists('pageYOffset', window, 'number')
+                ? window.pageYOffset
+                : null
+    }
+})()
